bd9ae7f0f5aade1b2fd2f35b84c9deea455d0fde
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-10-11 11:46:50 +0300

    Rework owned version

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ impl ZstdWrapper {
         Self {
             c_dict,
             d_dict,
-            buffer: Vec::with_capacity(1024 * 1024),
+            buffer: Vec::new(),
         }
     }
 
@@ -36,21 +36,38 @@ impl ZstdWrapper {
         Ok(&self.buffer[0..out_pos])
     }
 
-    pub fn compress_owned(&mut self, bytes: &[u8]) -> Result<Vec<u8>> {
-        self.compress(bytes).map(|x| x.to_vec())
+    pub fn compress_owned(&self, bytes: &[u8]) -> Result<Vec<u8>> {
+        let mut wrapper = io::Cursor::new(bytes);
+        let mut out_buffer = Vec::with_capacity(bytes.len());
+        let mut output_wrapper = io::Cursor::new(&mut out_buffer);
+
+        let mut encoder =
+            zstd::stream::Encoder::with_prepared_dictionary(&mut output_wrapper, &self.c_dict)?;
+        io::copy(&mut wrapper, &mut encoder)?;
+        encoder.finish()?;
+        Ok(out_buffer)
     }
 
-    pub fn decompress_owned(&mut self, bytes: &[u8]) -> Result<Vec<u8>> {
-        self.decompress(bytes).map(|x| x.to_vec())
+    pub fn decompress_owned(&self, bytes: &[u8]) -> Result<Vec<u8>> {
+        let mut wrapper = io::Cursor::new(bytes);
+        let mut out_buffer = Vec::with_capacity(bytes.len());
+        let mut output_wrapper = io::Cursor::new(&mut out_buffer);
+
+        let mut decoder =
+            zstd::stream::Decoder::with_prepared_dictionary(&mut wrapper, &self.d_dict)?;
+        io::copy(&mut decoder, &mut output_wrapper)?;
+        Ok(out_buffer)
     }
 
     pub fn decompress(&mut self, bytes: &[u8]) -> Result<&[u8]> {
         self.buffer.truncate(0);
         let mut wrapper = io::Cursor::new(bytes);
+        let mut output_wrapper = io::Cursor::new(&mut self.buffer);
+
         let mut decoder =
             zstd::stream::Decoder::with_prepared_dictionary(&mut wrapper, &self.d_dict)?;
-        let mut output_wrapper = io::Cursor::new(&mut self.buffer);
         io::copy(&mut decoder, &mut output_wrapper)?;
+
         let out_pos = output_wrapper.position() as usize;
         drop(output_wrapper);
         Ok(&self.buffer[0..out_pos])
@@ -115,6 +132,53 @@ mod test {
         println!("Len: {}", res.len());
 
         let got = encoder.decompress(&res).unwrap();
+
+        assert_eq!(expected, got);
+    }
+
+    #[test]
+    fn test_encode_owned() {
+        let encoder = ZstdWrapper::new();
+        let input = b"asasaasasasasasasasasasaaaaaaaaaaaaasassas";
+        let res = encoder.compress_owned(&input[..]).unwrap();
+        let mut de = Decoder::with_dictionary(&res[..], include_bytes!("../dictionary")).unwrap();
+        let mut out = vec![];
+        de.read_to_end(&mut out).unwrap();
+        assert_eq!(&out, input);
+    }
+
+    #[test]
+    fn test_encode_rand_owned() {
+        let encoder = ZstdWrapper::new();
+        let mut expected = vec![0; 1024 * 10];
+        rand::thread_rng().fill(expected.as_mut_slice());
+
+        let res = encoder.compress_owned(&expected).unwrap().to_vec();
+        println!("Len: {}", res.len());
+
+        let got = encoder.decompress_owned(&res).unwrap();
+
+        assert_eq!(expected, got);
+    }
+
+    #[test]
+    fn test_multiple_owned() {
+        let encoder = ZstdWrapper::new();
+        let mut expected = vec![0; 1024 * 1024 * 8];
+        rand::thread_rng().fill(expected.as_mut_slice());
+
+        let res = encoder.compress_owned(&expected).unwrap().to_vec();
+        println!("Len: {}", res.len());
+
+        let got = encoder.decompress_owned(&res).unwrap();
+
+        assert_eq!(expected, got);
+
+        rand::thread_rng().fill(expected.as_mut_slice());
+        let res = encoder.compress_owned(&expected).unwrap().to_vec();
+        println!("Len: {}", res.len());
+
+        let got = encoder.decompress_owned(&res).unwrap();
 
         assert_eq!(expected, got);
     }
